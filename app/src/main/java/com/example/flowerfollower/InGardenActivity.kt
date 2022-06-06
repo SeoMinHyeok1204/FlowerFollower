d8a7588afa337be9ec97c1ec7d96e404f6b64bb1
SeoMinHyeok1204 <80433383+SeoMinHyeok1204@users.noreply.github.com>
2022-06-06 17:06:25 +0900

    마이 가든 꽃 클릭 화면 변경

--- app/src/main/java/com/example/flowerfollower/InGardenActivity.kt
+++ app/src/main/java/com/example/flowerfollower/InGardenActivity.kt
@@ -6,16 +6,18 @@ import android.os.Bundle
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.flowerfollower.databinding.ActivityInGardenBinding
+import com.google.firebase.database.*
 
 class InGardenActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityInGardenBinding
+    lateinit var flowerDB : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInGardenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        flowerDB = FirebaseDatabase.getInstance().getReference("Flower")
         init()
     }
 
@@ -37,5 +39,36 @@ class InGardenActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+
+        flowerDB.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(data in snapshot.children) {
+                    val name = data.child("name").value as String
+                    if(name == flowerName) {
+                        val flowerLanguage = data.child("flowerlanguage").value as String
+                        val propagation = data.child("propagation").value as String
+                        val habitat = data.child("habitat").value as String
+                        val scientificName = data.child("scientificName").value as String
+                        val controlLevel = data.child("controlLevel").value as String
+                        val controlRequest = data.child("controlRequest").value as String
+                        val lightDemand = data.child("lightDemand").value as String
+
+                        binding.flowerLanguage.text = flowerLanguage
+                        binding.propagation.text = propagation
+                        binding.habitat.text = habitat
+                        binding.scientificName.text = scientificName
+                        binding.controlLevel.text = controlLevel
+                        binding.controlRequest.text = controlRequest
+                        binding.lightDemand.text = lightDemand
+
+                        return
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@InGardenActivity, "정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
